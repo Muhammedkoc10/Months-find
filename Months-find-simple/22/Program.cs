a04c5f9d53ee9fe1c73a456225b67fc4f493c695
Muhammed <muhammedkoc10@gmail.com>
2022-06-21 22:16:36 +0300

    Months-find

---
+++ Months-find-simple/22/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Kullanıcıdan alınan iki sayıdan önce girilen mi daha büyük sonra girilen mi büyük sonucu ekrana yazan program 
+            
+            Console.WriteLine("Birinci sayıyı giriniz");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("İkinci sayıyı giriniz");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi1 > sayi2)
+            {
+                Console.WriteLine("1. Girilen sayı daha büyüktür");
+            }
+            else if (sayi2 > sayi1)
+            {
+                Console.WriteLine("2. Girilen sayı daha büyüktür");
+            }
+            else
+            {
+                Console.WriteLine("Girilen iki sayı birbirine eşittir.");
+            }
+
+        }
+    }
+}
